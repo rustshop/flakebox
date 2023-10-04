eac9070a194325b83aca35b92bd2e32af235f5e2
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-10-03 20:49:55 -0700

    fix: ios cross-compilation on macos

--- lib/mkIOSToolchain.nix
+++ lib/mkIOSToolchain.nix
@@ -11,6 +11,12 @@ let
   target_underscores = lib.strings.replaceStrings [ "-" ] [ "_" ] target;
   target_underscores_upper = lib.strings.toUpper target_underscores;
 
+  # make bindgen (clang-sys) crate use /usr/bin/clang instead of NixOS
+  # clang
+  # https://github.com/KyleMayes/clang-sys/blob/b7992e864eaa2c8cb5c014a52074b962f257e29b/src/support.rs#L55
+  bindgen-clang = pkgs.writeShellScriptBin "${target}-clang" ''
+    exec /usr/bin/clang "$@"
+  '';
 in
 mkFenixToolchain {
   componentTargets = [ target ];
@@ -21,6 +27,8 @@ mkFenixToolchain {
     ## cc or ld?
     "LD_${target_underscores}" = "/usr/bin/cc";
     "CARGO_TARGET_${target_underscores_upper}_LINKER" = "/usr/bin/clang";
+
+    nativeBuildInputs = [ bindgen-clang ];
   };
 }
 
