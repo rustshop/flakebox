f75e504fc0251641c533d2109f66e4335b2a3f75
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-10-18 14:37:28 -0700

    feat: flakeboxLib.pickBinary

---
+++ lib/pickBinary.nix
@@ -0,0 +1,21 @@
+{ pkgs
+, system
+}:
+# Create a package that contains only one `bin`ary from an input `pkg`
+#
+# For efficiency we built some binaries together (like fedimintd + fedimint-cli),
+# but we would like to expose them separately.
+{ pkg, bin }:
+pkgs.stdenv.mkDerivation {
+  inherit system;
+  name = bin;
+
+  dontUnpack = true;
+  # just don't mess with it
+  dontStrip = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -a ${pkg}/bin/${bin} $out/bin/${bin}
+  '';
+}
