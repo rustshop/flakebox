547cb298f705f429e0e2e06cbdaed45e0307b8c6
Taliyah Webb <taliyahmail@proton.me>
2024-08-08 18:15:15 +0200

    ci(pre-commit): adds indicator to `cargo update --workspace --locked` output
    The output that the `pre-commmit` hook, and in extension `just lint` produces
    when this command is run cannot be attributed to cargo for first time users.
    This commit adds marker at the end of the line indicating the command output stems from (cargo)

--- lib/modules/cargo.nix
+++ lib/modules/cargo.nix
@@ -20,7 +20,7 @@
         git.pre-commit.hooks = {
           cargo_lock = ''
             # https://users.rust-lang.org/t/check-if-the-cargo-lock-is-up-to-date-without-building-anything/91048/5
-            flakebox-in-each-cargo-workspace cargo update --workspace --locked
+            flakebox-in-each-cargo-workspace cargo update --workspace --locked |& while read -r note ; do echo "$note    (cargo)"; done
           '';
         };
       })
